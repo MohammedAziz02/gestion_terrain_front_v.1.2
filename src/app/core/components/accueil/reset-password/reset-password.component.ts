262f657a633265a374d5a2fc5dc15710c6a528dd
Mohammed Aziz <64214635+MohammedAziz02@users.noreply.github.com>
2023-08-22 18:25:15 +0100

    reset-password works

--- src/app/core/components/accueil/reset-password/reset-password.component.ts
+++ src/app/core/components/accueil/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+
+  constructor(public userService : UserService) { }
+
+  messageerror : string = "";
+  messagesuccess : string = "";
+  academicEmail: string = "";
+
+
+  handleResetPassword(form : NgForm){
+    this.userService.resetPassword(form.value.email).subscribe(
+      (res : any) => {
+        this.messagesuccess = res.message;
+        this.messageerror = "";
+        this.userService.setIsLoading(false);
+      },
+      (err : any) => {
+        this.messageerror = err.error.message;
+        this.messagesuccess = "";
+        this.userService.setIsLoading(false);
+      }
+    )
+  }
+
+  goback(){
+    window.history.back();
+  }
 
 }
